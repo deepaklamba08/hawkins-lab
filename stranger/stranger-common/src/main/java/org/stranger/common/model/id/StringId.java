4924d7e108d1d664f6e65905803b95638d6a5871
deepak <deepaklamba.08@gmail.com>
2023-03-10 15:30:37 +0530

    id added

---
+++ stranger/stranger-common/src/main/java/org/stranger/common/model/id/StringId.java
@@ -0,0 +1,29 @@
+package org.stranger.common.model.id;
+
+import java.util.Objects;
+
+public class StringId implements Id{
+    private final String value;
+
+    public StringId(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return  this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StringId stringId = (StringId) o;
+        return Objects.equals(value, stringId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
